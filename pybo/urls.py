078fe78457d69d0e3a0525df5843519f0a51d4e7
kmgyu <lidersy961@gmail.com>
2025-01-12 22:37:58 +0900

    [feat] url nickname

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'pybo' # for url nickname redundunt problem
+
 # path에 url을 parameter로 받을 수 있게 처리하는 것도 여기서 함.
 urlpatterns = [
-    path("", views.index),
-    path('<int:question_id>/', views.detail)
+    path("", views.index, name="index"), # name : url 별칭(nickname?)
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 # Create your views here.
 
@@ -9,12 +9,14 @@ from .models import Question
 #     <h1>Hello, Django</h1>
 #                         """)
 
+# generic view를 이용 가능. 간단한데 @autowired같은 느낌인가?
 def index(request):
     question_list = Question.objects.order_by("-create_date")
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
