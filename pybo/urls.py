3f69402df69da5237f6c19214e43309e60fa8755
kmgyu <lidersy961@gmail.com>
2025-01-12 22:45:31 +0900

    answer crud - read, save - answer create, save

--- pybo/urls.py
+++ pybo/urls.py
@@ -7,4 +7,5 @@ app_name = 'pybo' # for url nickname redundunt problem
 urlpatterns = [
     path("", views.index, name="index"), # name : url 별칭(nickname?)
     path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 # Create your views here.
 
 # from django.http import HttpResponse
@@ -11,12 +12,32 @@ from .models import Question
 
 # generic view를 이용 가능. 간단한데 @autowired같은 느낌인가?
 def index(request):
+    '''
+    index pages
+    '''
     question_list = Question.objects.order_by("-create_date")
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
+    '''
+    detail page of question and answer
+    '''
     question = get_object_or_404(Question, pk=question_id)
     # question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    '''
+    pybo answer register
+    '''
+    question = get_object_or_404(Question, pk=question_id)
+    # same as below
+    # question.answer_set.create(content=request.POST.get('content'),
+    #                            create_date=timezone.now(),
+    #                            modify_date=timezone.now())
+    answer = Answer(question=question, content=request.POST.get('content'),
+                    create_date=timezone.now(), modify_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question.id)
