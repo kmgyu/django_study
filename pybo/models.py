995402c18ca61d72bd285364c756b9ba4bb62f66
kmgyu <lidersy961@gmail.com>
2025-01-28 22:36:11 +0900

    [fix] 답변 안보이는 문제 수정 - foreign key에 author 연결 안된 문제 수정

--- pybo/models.py
+++ pybo/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     # jump to django에서는 makemigrations할 때 1번 옵션(null=True) 해줬는데... 난 나보다 약한 녀석의 말은 따르지 않는다
     # 2번은 계정 정보를 아무거나 집어넣는 거임.
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='author_question')
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -17,8 +17,8 @@ class Question(models.Model):
 
 class Answer(models.Model):
     # question foreign key
-    question = models.ForeignKey(Question, on_delete=models.CASCADE,
-                                 related_name='author_answer')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
